7c9f8dbc0da78bd62bb93e1e415b833a6236ee71
yisaleh <yara_ahmed_saleh@yahoo.com>
2021-05-18 11:18:57 +0200

    deploying

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const path =require('path');
 const express= require('express');
-const port =process.env.PORT || 3000;
+// const port =process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname, '../public');
 var app=express();
