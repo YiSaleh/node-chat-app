ea1a85e30b3e59e01409345d1150863df6662d84
yisaleh <yara_ahmed_saleh@yahoo.com>
2021-05-18 12:40:57 +0200

    deploy

--- server/server.js
+++ server/server.js
@@ -5,6 +5,6 @@ const port =process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 var app=express();
 app.use(express.static(publicPath));
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('app is running on ${port}'); 
 });
